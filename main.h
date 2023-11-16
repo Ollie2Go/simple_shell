fd293df918b9d1fef4ea34b7c3ed3714b54b654a
Ollie2Go <olwethungxatu@gmail.com>
2023-11-16 09:17:27 -0800

    Betty code checker passed.

--- main.h
+++ main.h
@@ -1,10 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * This is my header files.
+ *
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+
+/*This is my functions. */
 
 int main(void);
 
